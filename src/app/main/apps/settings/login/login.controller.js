e6e76ebdd901cfc7cf103f45bb7796e8a4144927
Nwachukwu Ugwuanyi <codedeus>
2018-02-04 18:34:43 +0100

    second commit

--- src/app/main/apps/settings/login/login.controller.js
+++ src/app/main/apps/settings/login/login.controller.js
@@ -2,7 +2,7 @@
     'use strict';
 
     angular.module("app.settings.login")
-      .controller('LoginController', function($scope, $rootScope, $state,$http,$cookieStore) {
+      .controller('LoginController', function($scope, $rootScope, $state,$http,$cookieStore,$mdDialog) {
         var vm = this;
         $rootScope.globals = $rootScope.globals || {};
         $rootScope.globals.currentUser = $rootScope.globals.currentUser || {};
@@ -12,6 +12,7 @@
         };
 
         vm.login = function() {
+          $scope.processingRequest = true;
           vm.form = {
             email : vm.username,
             password : vm.password
@@ -21,6 +22,7 @@
           .post("https://murmuring-springs-18419.herokuapp.com/login", vm.form)
           .then(function(response) {
             if (response.data !== "" && response.data != null) {
+              $scope.processingRequest = false;
               $http.defaults.headers.common['X-AUTH-TOKEN'] = response.data.token;
               $cookieStore.put("loggedInUser", response.data);
               localStorage.setItem("loggedInUser", JSON.stringify(response.data));
@@ -28,6 +30,25 @@
 
               $state.go('app.dashboard');
             }
+            else{
+              $scope.processingRequest = false;
+              $mdDialog.show(
+                $mdDialog.alert()
+                .parent(angular.element(document.querySelector('#popupContainer')))
+                .title('Error!!!')
+                .textContent('Invalid Username and or password.')
+                .ariaLabel('Alert Dialog Demo')
+                .ok('Got it!'));
+            }
+          },function(error){
+            $scope.processingRequest = false;
+              $mdDialog.show(
+                $mdDialog.alert()
+                .parent(angular.element(document.querySelector('#popupContainer')))
+                .title('Error!!!')
+                .textContent('Invalid Username and or password.')
+                .ariaLabel('Alert Dialog Demo')
+                .ok('Got it!'));
           });
         };
     });
