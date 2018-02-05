67b266cfa7c3df2be3979059a0641a46b78673fd
Ogbuanya Austine <diribe3000@gmail.com>
2018-02-05 01:46:16 +0100

    Added manageAppraisal

--- src/app/main/apps/appraisals/appraisals.module.js
+++ src/app/main/apps/appraisals/appraisals.module.js
@@ -1,21 +1,28 @@
-(function() {
-  'use strict';
+;(function() {
+  "use strict"
 
-  angular.module('app.appraisals', [
-          'app.appraisals.newappraisal'
-      ])
-      .config(config);
+  angular
+    .module("app.appraisals", [
+      "app.appraisals.newappraisal",
+      "app.appraisals.manageappraisal"
+    ])
+    .config(config)
 
   function config(msNavigationServiceProvider) {
-      msNavigationServiceProvider.saveItem('appraisals', {
-          title: 'Appraisals',
-          weight: 1,
-          icon: 'icon-cog'
-      });
+    msNavigationServiceProvider.saveItem("appraisals", {
+      title: "Appraisals",
+      weight: 1,
+      icon: "icon-cog"
+    })
 
-      msNavigationServiceProvider.saveItem('appraisals.newappraisal', {
-        title: 'New Appraisal',
-        state: 'app.newappraisal'
-    });
+    msNavigationServiceProvider.saveItem("appraisals.newappraisal", {
+      title: "New Appraisal",
+      state: "app.newappraisal"
+    })
+
+    msNavigationServiceProvider.saveItem("appraisals.manageappraisal", {
+      title: "Manage Appraisal",
+      state: "app.manageappraisal"
+    })
   }
-})();
+})()
---
+++ src/app/main/apps/appraisals/manageappraisal/manageappraisal.module.js
@@ -0,0 +1,26 @@
+(function() {
+  "use strict"
+
+  angular.module("app.appraisals.manageappraisal", []).config(config)
+
+  function config($stateProvider, $translatePartialLoaderProvider) {
+    $stateProvider.state("app.manageappraisal", {
+      url: "/manageappraisal",
+      data: {
+        name: "Manage Appraisal",
+        role: "manageappraisal"
+      },
+      views: {
+        "content@app": {
+          templateUrl:
+            "app/main/apps/appraisals/manageappraisal/manageappraisal.html",
+          controller: "ManageAppraisalController as vm"
+        }
+      }
+    })
+
+    $translatePartialLoaderProvider.addPart(
+      "app/main/apps/appraisals/manageappraisal"
+    )
+  }
+})()
